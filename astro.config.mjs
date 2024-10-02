5faa4c442eeb01f23af03583f3d8bbdfee5d242d
wernerkotze <wernerkotze.rsa@gmail.com>
2024-10-02 08:20:38 +0200

    updated config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://vbartalis.github.io",
-  base: "/home"
+  site: "https://vbartalis.github.io"
 });
